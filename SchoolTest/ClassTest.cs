3289e1a71adf97de5532ebf1f452a8468d31a47d
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-19 18:13:01 +0530

    first commit

---
+++ SchoolTest/ClassTest.cs
@@ -0,0 +1,41 @@
+﻿using SchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTest
+{
+    [TestFixture]
+    public class ClassTests
+    {
+        [Test]
+        public void TestClassName()
+        {
+            // Arrange
+            Class myClass = new Class();
+            myClass.Name = "Maths";
+
+            // Act
+            string className = myClass.Name;
+
+            // Assert
+            Assert.AreEqual("Maths", className);
+        }
+
+        [Test]
+        public void TestClassId()
+        {
+            // Arrange
+            Class myClass = new Class();
+            myClass.Id = 1;
+
+            // Act
+            int classId = myClass.Id;
+
+            // Assert
+            Assert.AreEqual(1, classId);
+        }
+    }
+}
---
+++ SchoolTest/StudentTest.cs
@@ -0,0 +1,57 @@
+﻿using SchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTest
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        [Test]
+        public void TestStudentId()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Id = 1;
+
+            // Act
+            int studentId = student.Id;
+
+            // Assert
+            Assert.AreEqual(1, studentId);
+        }
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "Sam";
+
+            // Act
+            string studentName = student.Name;
+
+            // Assert
+            Assert.AreEqual("Sam", studentName);
+        }
+
+        [Test]
+        public void TestStudentClass()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Class = 5;
+
+            // Act
+            int studentClass = student.Class;
+
+            // Assert
+            Assert.AreEqual(5, studentClass);
+        }
+
+
+
+    }
+}
